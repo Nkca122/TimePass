3ecef1bca43b304051d72aebc31646a38ea298b5
Nkca122 <nkca122@gmail.com>
2024-05-15 01:23:57 +0530

    init

---
+++ web/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const app = express();
+const path = require("path");
+
+let port = 8000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.listen(port, ()=>{
+    console.log(`The server is on ${port}`);
+});
+
+//Homepage to select the tye of form
+
+app.get("/form/new", (req, res)=>{
+    res.render("home.ejs");
+});
+
+let Num;
+let formType;
+
+//To go to the new form
+
+app.post("/form" ,(req,res)=>{
+    let {num, type} = req.body;
+    Num = Number(num);  formType = type;
+    res.redirect("/form");
+});
+
+app.get("/form", (req, res)=>{
+    res.render("form.ejs", {Num, formType});
+});
+
+
+
